50a90234aa26c918887279f9ee401172abe307a8
Philippe Leclerc <philippeleclerc299@gmail.com>
2019-12-19 00:17:37 -0500

    Removed table annotation on one and two hand swords

--- poe-backend/Models/ItemData/Weapons/OneHandedSword.cs
+++ poe-backend/Models/ItemData/Weapons/OneHandedSword.cs
@@ -1,8 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace poe_backend.Models.ItemData.Weapons
+﻿namespace poe_backend.Models.ItemData.Weapons
 {
-    [Table("One_Hand_Swords")]
     public class OneHandedSword : BaseItem
     {
         public override ItemClass ItemClass => ItemClass.OneHandedSword;
--- poe-backend/Models/ItemData/Weapons/TwoHandedSword.cs
+++ poe-backend/Models/ItemData/Weapons/TwoHandedSword.cs
@@ -1,8 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace poe_backend.Models.ItemData.Weapons
+﻿namespace poe_backend.Models.ItemData.Weapons
 {
-    [Table("Two_Hand_Swords")]
     public class TwoHandedSword : BaseItem
     {
         public override ItemClass ItemClass => ItemClass.TwoHandedSword;
