e8f3f3f18eed82cb7ff82b9128113c88b935c163
Philippe Leclerc <philippeleclerc299@gmail.com>
2019-12-19 00:16:38 -0500

    Added sets for all base since now

--- poe-backend/Database/PoeAppDbContext.cs
+++ poe-backend/Database/PoeAppDbContext.cs
@@ -8,17 +8,38 @@ namespace poe_backend.Database
 {
     public class PoeAppDbContext : DbContext
     {
-        public DbSet<OneHandedSword> OneHandedSwords { get; set; }
-        public DbSet<TwoHandedSword> TwoHandedSwords { get; set; }
-        public DbSet<Amulet> Amulets { get; set; }
-        public DbSet<Talisman> Talismans { get; set; }
+        //Armours
         public DbSet<BodyArmour> BodyArmours { get; set; }
         public DbSet<Boot> Boots { get; set; }
         public DbSet<Glove> Gloves { get; set; }
         public DbSet<Helmet> Helmets { get; set; }
+        public DbSet<Quiver> Quivers { get; set; }
+        public DbSet<Shield> Shields { get; set; }
 
+        // Jewelery
+        public DbSet<Amulet> Amulets { get; set; }
+        public DbSet<Talisman> Talismans { get; set; }
+        public DbSet<Ring> Rings { get; set; }
+        public DbSet<Belt> Belts { get; set; }
+
+        // Weapons
+        public DbSet<Bow> Bows { get; set; }
+        public DbSet<Claw> Claws { get; set; }
+        public DbSet<Dagger> Daggers { get; set; }
+        public DbSet<FishingRod> FishingRods { get; set; } // fishing rods o_O ok poe ..
+        public DbSet<OneHandedAxe> OneHandedAxes { get; set; }
+        public DbSet<OneHandedMace> OneHandedMaces { get; set; }
+        public DbSet<OneHandedSword> OneHandedSwords { get; set; }
+        public DbSet<OneHandedThrustingSword> OneHandedThrustingSwords { get; set; }
+        public DbSet<Staff> Staves { get; set; }
+        public DbSet<TwoHandedAxe> TwoHandedAxes { get; set; }
+        public DbSet<TwoHandedMace> TwoHandedMaces { get; set; }
+        public DbSet<TwoHandedSword> TwoHandedSwords { get; set; }
+        public DbSet<Wand> Wands { get; set; }
+
+
+        // Others
         public DbSet<PoeTag> PoeTags { get; set; }
-
         public DbSet<ItemTag> ItemTags { get; set; }
 
         public PoeAppDbContext() : base(new DbContextOptionsBuilder<PoeAppDbContext>()
